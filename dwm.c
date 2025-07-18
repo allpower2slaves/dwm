6a320f223e10b37a63f8d3304c7820f8a0126cb5
allpower2slaves <allpower2slaves@users.noreply.github.com>
2025-07-18 23:08:09 +0300

    added alpha patch II hotfix edition

--- dwm.c
+++ dwm.c
@@ -252,7 +252,7 @@ static Monitor *wintomon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
-static void xinitvisual();
+static void xinitvisual(void);
 static void zoom(const Arg *arg);
 
 static unsigned int nexttag(void);
@@ -2252,7 +2252,7 @@ xerrorstart(Display *dpy, XErrorEvent *ee)
 }
 
 void
-xinitvisual()
+xinitvisual(void)
 {
 	XVisualInfo *infos;
 	XRenderPictFormat *fmt;
