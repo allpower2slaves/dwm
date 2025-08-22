bf857c257875d497887a026197c464497b11c06d
allpower2slaves <allpower2slaves@users.noreply.github.com>
2025-08-23 00:19:57 +0300

    super+c no longer kills clients

--- config.h
+++ config.h
@@ -134,7 +134,7 @@ static const Key keys[] = {
 	{ MODKEY|Mod4Mask|ShiftMask,              XK_Home,      togglegaps,     {0} },
 	//{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
